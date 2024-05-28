73f7004e14fb50bdd1f433b74c27006ef3373087
KubaJerz <62904217+KubaJerz@users.noreply.github.com>
2024-05-28 11:00:45 -0400

    tree now has gini

--- decision_tree/d_tree.py
+++ decision_tree/d_tree.py
@@ -6,29 +6,33 @@ import pandas as pd
 
 class DecisionTree():
 
-    def __init__(self, max_depth = 5, min_samples = 1, min_info_gain = 0.0) -> None:
+    def __init__(self, max_depth = 5, min_samples = 1, min_info_gain = 0.0, criterion = 'gini') -> None:
         self.max_depth = max_depth #mac depth the tree can be
         self.min_samples = min_samples #min samples allowed in leaf for us to continues to split on it 
         self.min_info_gain = min_info_gain #min info gain needed to continue to build the tree
         self.labels = None
+        self.criterion = criterion
 
 
     def __entrpoy(self, all_labels: list) -> float:
         class_prob = [label_count / len(all_labels) for label_count in Counter(all_labels).values()] #this well calc the prob each class being randomly setlected so len(set) = 10, five 0's, five 1's then class_prob = [.5,.5]
-        return -1 * sum([p * math.log(p,2) for p in class_prob])
+        return -1 * sum(p * math.log(p,2) for p in class_prob)
     
     def __gini(self, all_labels: list) -> float:
         class_prob = [label_count / len(all_labels) for label_count in Counter(all_labels).values()]
-        return sum(prob * (1 -  prob) for prob in class_prob)
+        return (1 - sum(prob ** 2 for prob in class_prob))
     
 
     '''
     This will return the sum weighted entropy for two lists
     '''
     def __sets_entropy(self, splits: list) -> float:
-        total = sum([len(split) for split in splits])
+        total = sum(len(split) for split in splits)
         return sum(len(split)/total * self.__entrpoy(split) for split in splits) # we get the entropy of each split then weight it by its size compared to the total size of the splits then sum
 
+    def __sets_gini(self, splits: list) -> float:
+        total = sum(len(split) for split in splits)
+        return sum(len(split)/total * self.__gini(split) for split in splits)
 
     def __split(self, data, f_index, threashold):
         bool_mask = data[:, f_index] < threashold #this makes array where for each row it 0 or 1 if if meet or did not the threashhold
@@ -41,8 +45,9 @@ class DecisionTree():
     Given some data the best split will be on the feature that returns two sets with the lowest entrpoy.
     '''
     def __find_best_split(self, data: np.array) -> tuple:
+        criterion = self.criterion
         
-        b_s_entropy = float('inf')
+        b_s = float('inf')
 
         features_data = data[:, :-1] #last col is labels
 
@@ -55,29 +60,38 @@ class DecisionTree():
                 data_left, data_right = self.__split(data, f_idx, possible_threashold)
 
                 if len(data_left) > 0 and len(data_right) > 0:
-                    split_entropy = self.__sets_entropy([data_left[:, -1], data_right[:, -1]])#we send in two lists each of the labels of each side of the split and calcualte there sum entropy
+                    if criterion == 'entropy':   #entropy is the criterion
+                        split_gain = self.__sets_entropy([data_left[:, -1], data_right[:, -1]])#we send in two lists each of the labels of each side of the split and calcualte there sum entropy
+                    else:    #entropy is the gini
+                        split_gain = self.__sets_gini([data_left[:, -1], data_right[:, -1]])#we send in two lists each of the labels of each side of the split and calcualte there sum entropy
 
-                    if split_entropy < b_s_entropy: #the entropy of the set split we just found was better than all the other splits we have seen so far. 
-                        b_s_entropy = split_entropy
+                    if split_gain < b_s: #the entropy of the set split we just found was better than all the other splits we have seen so far. 
+                        b_s = split_gain
                         data_right_best = data_right
                         data_left_best = data_left
                         best_f_idx = f_idx
                         best_f_val = possible_threashold
 
-        return data_left_best, data_right_best, best_f_idx, best_f_val, b_s_entropy
+        return data_left_best, data_right_best, best_f_idx, best_f_val, b_s
     
 
     '''
     builds the tree by recursivly doing the best (greedy) split until one of the stopping conditions have been met 
     '''
     def __build_tree(self, data: np.array, curr_depth: int) -> TreeNode:
+        criterion = self.criterion
+
         if curr_depth >= self.max_depth:
             return None
         
-        s_data_left, s_data_right, s_f_idx, s_f_val, s_entropy = self.__find_best_split(data) #find the ebst split and info form the split
+        s_data_left, s_data_right, s_f_idx, s_f_val, split_entropy_or_gini = self.__find_best_split(data) #find the ebst split and info form the split
 
-        curr_entropy = self.entrpoy(data[:, -1]) #gets the entropy of the curr data 
-        info_gain = curr_entropy - s_entropy #calc the information gain from the best split on this data
+        if criterion == 'entropy':
+            curr_entropy = self.__entrpoy(data[:, -1]) #gets the entropy of the curr data 
+            info_gain = curr_entropy - split_entropy_or_gini #calc the information gain from the best split on this data
+        else:
+            curr_gini = self.__gini(data[:, -1]) #gets the gini of the curr data 
+            info_gain = curr_gini - split_entropy_or_gini #calc the information gain from the best split on this data
 
         node = TreeNode(data, s_f_idx, s_f_val, info_gain)
 
